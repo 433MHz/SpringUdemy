9e5403ace1d555af04b112ee2a36bd59f5083ec9
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-05-07 11:39:54 +0200

    Example of using setter injection

--- SpringUdemy/src/main/java/pl/krystian/SpringUdemy/TrackCoach.java
+++ SpringUdemy/src/main/java/pl/krystian/SpringUdemy/TrackCoach.java
@@ -2,8 +2,15 @@ package pl.krystian.SpringUdemy;
 
 public class TrackCoach implements Coach {
 
+	private HappyFortuneService service;
+	
 	public String getText() {
-		return "Track coach";
+		return "Track coach " + service.getFortune();
 	}
 
+	public void setService(HappyFortuneService service) {
+		this.service = service;
+	}
+
+	
 }
